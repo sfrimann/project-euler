556ceafd6abce2c7d2161b6a6256c8ed8e8de858
fractalleaf <sfrimann@gmail.com>
2019-03-31 12:54:19 +0200

    Added solution to problem 657

---
+++ p657/p657.py
@@ -0,0 +1,24 @@
+def I(alpha, n, mod=10**9+7):
+    alphaCn = alpha % mod # initialize to alpha choose 1
+    n_incomplete = 0
+    for i in range(1, alpha+1):
+        if i == (alpha - 1):
+            power_sum = (n + 1) % mod
+        else:
+            power_sum = ((1 - pow(alpha-i, n+1, mod)) * pow((1 - alpha + i), mod-2, mod)) % mod
+
+        n_incomplete += (pow(-1, i+1, mod) * alphaCn * power_sum) % mod
+        n_incomplete %= mod
+
+        # update binomial coefficient recursively
+        alphaCn *= (alpha - i) * pow(i+1, mod-2, mod)
+        alphaCn %= mod
+
+    return n_incomplete
+
+if __name__ == '__main__':
+    print("")
+    print("Project Euler 657 solution")
+    MOD = 10**9+7
+    for ALPHA, N in zip([3, 3, 3, 10**7], [0, 2, 4, 10**12]):
+        print("I({},{}) mod {} = {}".format(ALPHA, N, MOD, I(ALPHA, N, mod=MOD)))
